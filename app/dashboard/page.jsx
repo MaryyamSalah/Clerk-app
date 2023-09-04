b2fc9d8ccfe593a5f1d5fc0b08a62a186ee53f5e
MaryyamSalah <maryyamsalah20@gmail.com>
2023-09-04 22:36:55 +0300

    protect-pages

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <>
+    <h1 className=" font-bold text-2xl mb-5">Dashboard</h1>
+    <p className="mb-5">welcome to the dashboard</p>
+    
+    </>
+
+  )
+}
+
+export default Dashboard
