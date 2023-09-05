0fc03484efb5559bb366661065efdcb020335915
MaryyamSalah <maryyamsalah20@gmail.com>
2023-09-05 22:27:55 +0300

    adding user auth & user button

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,8 @@
 export default function Home() {
   return (
     <>
-    <h1>Home</h1>
+     <h1 className=" font-bold text-2xl mb-5">Home</h1>
+    <p className="mb-5">welcome to the Home</p>
     </>
   )
 }
